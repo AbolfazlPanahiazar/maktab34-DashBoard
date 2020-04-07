cbed4cbd278b793b9fdcf29a371bf70108cab771
AbolfazlPanahiazar <55955710+AbolfazlPanahiazar@users.noreply.github.com>
2020-04-07 10:58:51 +0430

    add chart

--- js/index.js
+++ js/index.js
@@ -9,3 +9,31 @@ $(window).scroll(function() {
       "fast"
     );
 });
+
+new Chart(document.getElementById("line-chart"), {
+  type: "line",
+  data: {
+    labels: [0, 5, 10, 15, 20, 25, 30],
+    datasets: [
+      {
+        data: [0, 17, 20, 33, 10, 48, 10, 18],
+        label: "Tickets",
+        borderColor: "#f39c1b",
+        fill: true,
+        backgroundColor: "rgba(255, 233, 218, 0.6)"
+      }
+    ]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          drawOnChartArea: false
+        }
+      }]
+    }
+  }
+});
